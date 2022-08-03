8633b33189b30e070ae81bb919d470728578898f
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-08-03 18:21:50 +0200

    Move the introduction to README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+## Welcome!
+
+This is an optimization model for a fully intermittent renewable Pan-European electricity grid. The model aims to find the least-cost solution for the deployment of solar PV, wind energy and storage. By overbuilding generation capacity to some degree, significant less storage capacity is required, resulting in lower system costs.
+
+The demand and climate data used in this model are from ENTSO-E's 2021 European Resource Adequacy Assessment (ERAA) and can be downloaded [here](https://www.entsoe.eu/outlooks/eraa/2021/eraa-downloads/). The techno-economic data is from NREL's Annual Technology Baseline and can be found [here](https://atb.nrel.gov/).
+
+The source code of the model can be found on [Github](https://github.com/RubenVanEldik/thesis-model). It consists out of three parts, the preprocessing, optimization, and analysis.
+
+### Preprocessing
+The preprocessor only needs to be run once. It will convert all the hourly ERAA data into a format that can be parsed more easily by the optimization.
+
+### Optimization
+...
+
+### Analysis
+...
--- 🌤️_Introduction.py
+++ 🌤️_Introduction.py
@@ -1,28 +1,12 @@
 import streamlit as st
 
+import utils
 
+# Show Utrecht University logo
 st.image("./images/logo.png")
 
-st.markdown(
-    """
-    ## Welcome!
-
-    This is an optimization model for a fully intermittent renewable Pan-European electricity grid. The model aims to find the least-cost solution for the deployment of solar PV, wind energy and storage. By overbuilding generation capacity to some degree, significant less storage capacity is required, resulting in lower system costs.
-
-    The demand and climate data used in this model are from ENTSO-E's 2021 European Resource Adequacy Assessment (ERAA) and can be downloaded [here](https://www.entsoe.eu/outlooks/eraa/2021/eraa-downloads/). The techno-economic data is from NREL's Annual Technology Baseline and can be found [here](https://atb.nrel.gov/).
-
-    The source code of the model can be found on [Github](https://github.com/RubenVanEldik/thesis-model). It consists out of three parts, the preprocessing, optimization, and analysis.
-
-    ### Preprocessing
-    The preprocessor only needs to be run once. It will convert all the hourly ERAA data into a format that can be parsed more easily by the optimization.
-
-    ### Optimization
-    ...
-
-    ### Analysis
-    ...
-"""
-)
+# Show the README introduction
+st.markdown(utils.read_text(utils.path("README.md")))
 
 
 st.markdown(
