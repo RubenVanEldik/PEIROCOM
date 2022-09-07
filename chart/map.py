9adbdbee9ea52db22d39c5347f12bf648723d095
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-09-07 11:21:23 +0200

    Show countries with no data as white on the map

--- chart/map.py
+++ chart/map.py
@@ -33,7 +33,7 @@ class Map:
         map_df["data"] = map_df.index.map(data)
 
         # Plot the data
-        map_df.plot(column="data", cmap=colormap, linewidth=0.5, ax=self.ax, edgecolor=colors.get("gray", 600))
+        map_df.plot(column="data", cmap=colormap, linewidth=0.5, ax=self.ax, edgecolor=colors.get("gray", 600), missing_kwds={"color": "white"})
 
     def save(self, filepath):
         plt.savefig(filepath, dpi=2500, bbox_inches="tight", pad_inches=0.2)
