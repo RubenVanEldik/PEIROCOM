58e6414aee112adb843bdf049250d52e36c7efda
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-10-05 17:44:40 +0200

    Add a script to upload the Docker container

---
+++ scripts/docker.upload.sh
@@ -0,0 +1,11 @@
+echo "Username:"
+read user_id
+
+echo "IP address:"
+read server
+
+cd ..
+scp .env $user_id@$server:Documents
+scp gurobi.lic $user_id@$server:Documents
+scp thesis-model.tar.gz $user_id@$server:Documents
+scp -r scripts $user_id@$server:Documents
