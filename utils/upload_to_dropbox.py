600516f3e6e18a38abcfccf880e709f3d07b114c
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-10-07 13:01:45 +0200

    Use files_upload_session to upload files to Dropbox

--- utils/upload_to_dropbox.py
+++ utils/upload_to_dropbox.py
@@ -11,6 +11,28 @@ if dropbox_app_key and dropbox_app_secret and dropbox_refresh_token:
     client = dropbox.Dropbox(app_key=dropbox_app_key, app_secret=dropbox_app_secret, oauth2_refresh_token=dropbox_refresh_token)
 
 
+def _upload_file(filepath, dropbox_path):
+    """
+    Upload a file to Dropbox
+    """
+    assert validate.is_filepath(filepath)
+    assert validate.is_directory_path(dropbox_path)
+
+    file_size = filepath.stat().st_size
+    chunk_size = 4 * 1024 * 1024
+
+    with open(filepath, "rb") as f:
+        upload_session_start_result = client.files_upload_session_start(f.read(chunk_size))
+        cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id, offset=f.tell())
+        commit = dropbox.files.CommitInfo(path=f"/{dropbox_path / filepath.name}", mute=True)
+        while f.tell() < file_size:
+            if (file_size - f.tell()) <= chunk_size:
+                client.files_upload_session_finish(f.read(chunk_size), cursor, commit)
+            else:
+                client.files_upload_session_append(f.read(chunk_size), cursor.session_id, cursor.offset)
+                cursor.offset = f.tell()
+
+
 def upload_to_dropbox(path, dropbox_path):
     """
     Upload a file or directory to Dropbox
@@ -25,6 +47,6 @@ def upload_to_dropbox(path, dropbox_path):
     # If the path is a directory upload the files as a ZIP file
     if path.is_dir():
         utils.zip(path)
-        client.files_upload(open(f"{path}.zip", "rb").read(), f"/{dropbox_path / path.name}.zip", mute=True)
+        _upload_file(path.parent / f"{path.name}.zip", dropbox_path)
     else:
-        client.files_upload(open(path, "rb").read(), f"/{dropbox_path / path.name}", mute=True)
+        _upload_file(path, dropbox_path)
