525b0b4678fca7e3f8d4823e0da9cf88965e4244
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-10-10 13:53:18 +0200

    Stop the container before removing it in docker.run.sh

--- scripts/docker.run.sh
+++ scripts/docker.run.sh
@@ -1,3 +1,4 @@
 cd ..
+docker container stop peirocom-container
 docker rm peirocom-container
 docker run -p=8501:8501 -v=$PWD/gurobi.lic:/opt/gurobi/gurobi.lic:ro --name peirocom-container peirocom
