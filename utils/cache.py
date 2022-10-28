2d45394c33ec069b56f916d29dd245a098f3e0ef
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-10-28 20:51:07 +0200

    Reduce the cache time for the demo

--- utils/cache.py
+++ utils/cache.py
@@ -3,8 +3,8 @@ import streamlit as st
 from .is_demo import is_demo
 
 
-# Set a maximum cache time of 5 minutes for the demo
-ttl = 5 * 60 if is_demo else None
+# Set a maximum cache time of 1 minute for the demo
+ttl = 60 if is_demo else 10
 
 # Remove the spinner and set the TTL of the Streamlit memo cache
 cache = st.experimental_memo(show_spinner=False, ttl=ttl)
