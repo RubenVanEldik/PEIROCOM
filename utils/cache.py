8a58783332bcc20ae186f857f004657a8ef89011
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-10-30 15:04:49 +0100

    Remove the TTL for the cache in non-demo mode

--- utils/cache.py
+++ utils/cache.py
@@ -4,7 +4,7 @@ from .is_demo import is_demo
 
 
 # Set a maximum cache time of 1 minute for the demo
-ttl = 60 if is_demo else 10
+ttl = 60 if is_demo else None
 
 # Remove the spinner and set the TTL of the Streamlit memo cache
 cache = st.experimental_memo(show_spinner=False, ttl=ttl)
