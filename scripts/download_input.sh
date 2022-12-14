0f6264ab73280150a83ec91d8612b5b086d3be96
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-12-14 01:09:49 +0100

    Fix the mkdir command in scripts/download_input.sh

--- scripts/download_input.sh
+++ scripts/download_input.sh
@@ -1,8 +1,7 @@
 # Download all bidding zone files
-mkdir "/app/input/bidding_zones"
 for scenario in "ERAA 2025" "ERAA 2030"
 do
-  mkdir "/app/input/scenarios/${scenario}/bidding_zones"
+  mkdir -p "/app/input/scenarios/${scenario}/bidding_zones"
   cd "/app/input/scenarios/${scenario}/bidding_zones"
   for bidding_zone in AL00 AT00 BA00 BE00 BG00 CH00 CY00 CZ00 DE00 DKE1 DKW1 EE00 ES00 FI00 FR00 FR15 GR00 GR03 HR00 HU00 IE00 ITCA ITCN ITCS ITN1 ITS1 ITSA ITSI LT00 LUB1 LUF1 LUG1 LV00 ME00 MK00 MT00 NL00 NOM1 NON1 NOS0 PL00 PT00 RO00 RS00 SE01 SE02 SE03 SE04 SI00 SK00 TR00 UA01 UK00 UKNI
   do
@@ -11,10 +10,9 @@ do
 done
 
 # Download all interconnection files
-mkdir "/app/input/interconnections"
 for scenario in "ERAA 2025" "ERAA 2030"
 do
-  mkdir "/app/input/scenarios/${scenario}/interconnections"
+  mkdir -p "/app/input/scenarios/${scenario}/interconnections"
   cd "/app/input/scenarios/${scenario}/interconnections"
   for interconnection_type in hvac hvdc limits
   do
