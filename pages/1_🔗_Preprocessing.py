c77fe2226e4903ff53cca2b40ac31f2f3953e88d
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-02-02 18:42:20 +0100

    Fix the climate data URL

--- pages/1_🔗_Preprocessing.py
+++ pages/1_🔗_Preprocessing.py
@@ -25,8 +25,8 @@ if st.button("Download demand data", disabled=demand_files_are_downloaded):
 climate_filenames = [utils.path("input", "eraa", "Climate Data", f"PECD_{generation_type}_{scenario['year']}_edition 2021.3.xlsx") for scenario in scenarios for generation_type in ["LFSolarPV", "Onshore", "Offshore"]]
 climate_files_are_downloaded = utils.validate_files(climate_filenames)
 if st.button("Download climate data", disabled=climate_files_are_downloaded):
-    interconnection_data_url = "https://eepublicdownloads.azureedge.net/clean-documents/sdc-documents/ERAA/Net%20Transfer%20Capacities.7z"
-    preprocessing.download_eraa_data(interconnection_data_url, interconnection_filenames)
+    climate_data_url = "https://eepublicdownloads.entsoe.eu/clean-documents/sdc-documents/ERAA/Climate%20Data.7z"
+    preprocessing.download_eraa_data(climate_data_url, climate_filenames)
 
 # Check if the interconnection files are downloaded
 interconnection_filenames = [utils.path("input", "eraa", "Transfer Capacities", f"Transfer Capacities_ERAA2021_TY{scenario['year']}.xlsx") for scenario in scenarios]
