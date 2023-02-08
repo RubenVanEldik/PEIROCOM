175adcea5c38715c5f02572babb5bb04ccf9f7cd
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-02-08 17:46:29 +0100

    Use 'inout' for tick directions

--- chart/__init__.py
+++ chart/__init__.py
@@ -7,3 +7,5 @@ from .sankey import Sankey
 # Set the font for all plots to serif
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["mathtext.fontset"] = "dejavuserif"
+plt.rcParams["xtick.direction"] = "inout"
+plt.rcParams["ytick.direction"] = "inout"
