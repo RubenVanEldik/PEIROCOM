d48ffd7baaa9970f7ae32f7897e1d47ae64615c8
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-02-23 15:32:36 +0100

    Add the technology types' colors to the technologies.yaml file

--- colors/__init__.py
+++ colors/__init__.py
@@ -64,10 +64,8 @@ def technology_type(technology_type, *, alpha=0.8):
     assert validate.is_technology_type(technology_type)
     assert validate.is_number(alpha, min_value=0, max_value=1)
 
-    if technology_type == "ires":
-        return get("blue", 600, alpha=alpha)
-    if technology_type == "storage":
-        return get("red", 600, alpha=alpha)
+    technology_type_color = utils.read_yaml(utils.path("input", "technologies.yaml"))[technology_type]["color"]
+    return get(technology_type_color["name"], technology_type_color["value"], alpha=alpha)
 
 
 def technology(technology_name, *, alpha=0.8):
