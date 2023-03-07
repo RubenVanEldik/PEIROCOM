5d07c3f7b938cec18b3e297b6eda9d9a2045473c
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-03-07 12:43:39 +0100

    Change 'client' to 'dropbox_client'

--- utils/upload_to_dropbox.py
+++ utils/upload_to_dropbox.py
@@ -9,7 +9,7 @@ dropbox_app_key = utils.get_env("DROPBOX_APP_KEY")
 dropbox_app_secret = utils.get_env("DROPBOX_APP_SECRET")
 dropbox_refresh_token = utils.get_env("DROPBOX_REFRESH_TOKEN")
 if dropbox_app_key and dropbox_app_secret and dropbox_refresh_token:
-    client = Dropbox(app_key=dropbox_app_key, app_secret=dropbox_app_secret, oauth2_refresh_token=dropbox_refresh_token)
+    dropbox_client = Dropbox(app_key=dropbox_app_key, app_secret=dropbox_app_secret, oauth2_refresh_token=dropbox_refresh_token)
 
 
 def _upload_file(filepath, dropbox_directory_path):
@@ -24,16 +24,16 @@ def _upload_file(filepath, dropbox_directory_path):
 
     with open(filepath, "rb") as f:
         if file_size <= chunk_size:
-            client.files_upload(f.read(), f"/{dropbox_directory_path / filepath.name}")
+            dropbox_client.files_upload(f.read(), f"/{dropbox_directory_path / filepath.name}")
         else:
-            upload_session_start_result = client.files_upload_session_start(f.read(chunk_size))
+            upload_session_start_result = dropbox_client.files_upload_session_start(f.read(chunk_size))
             cursor = UploadSessionCursor(session_id=upload_session_start_result.session_id, offset=f.tell())
             commit = CommitInfo(path=f"/{dropbox_directory_path / filepath.name}", mute=True)
             while f.tell() < file_size:
                 if (file_size - f.tell()) <= chunk_size:
-                    client.files_upload_session_finish(f.read(chunk_size), cursor, commit)
+                    dropbox_client.files_upload_session_finish(f.read(chunk_size), cursor, commit)
                 else:
-                    client.files_upload_session_append(f.read(chunk_size), cursor.session_id, cursor.offset)
+                    dropbox_client.files_upload_session_append(f.read(chunk_size), cursor.session_id, cursor.offset)
                     cursor.offset = f.tell()
 
 
@@ -44,7 +44,7 @@ def upload_to_dropbox(path, dropbox_directory_path):
     assert validate.is_directory_path(path) or validate.is_filepath(path)
     assert validate.is_directory_path(dropbox_directory_path)
 
-    if "client" not in globals():
+    if "dropbox_client" not in globals():
         print("Could not upload to Dropbox because DROPBOX_ACCESS_TOKEN was not set")
         return
 
