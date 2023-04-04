fc7861ea73cdf4b646235abe2edd015299e0b144
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-04-04 10:57:02 +0200

    Set the cache TTL to 120 seconds

--- utils/cache.py
+++ utils/cache.py
@@ -1,9 +1,4 @@
 import streamlit as st
 
-from .is_demo import is_demo
-
-# Set a maximum cache time of 1 minute for the demo
-ttl = 60 if is_demo else None
-
 # Remove the spinner and set the TTL of the Streamlit memo cache
-cache = st.cache_data(show_spinner=False, ttl=ttl)
+cache = st.cache_data(show_spinner=False, ttl=120)
