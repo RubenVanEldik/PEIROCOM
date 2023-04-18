5960d25ac373733ba28159ce3b06cfc3d0e83a48
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-04-18 15:15:00 +0200

    Set the minimum value of the potential capacity to the current capacity

--- utils/get_potential_per_ires_node.py
+++ utils/get_potential_per_ires_node.py
@@ -19,10 +19,6 @@ def get_potential_per_ires_node(market_node, ires_technology, *, mean_demand, co
     if ires_potential_country is None:
         return float("inf")
 
-    # Return 0 if there is no potential for this technology in this market node
-    if ires_potential_country == 0:
-        return 0
-
     # Calculate the potential in the market node based upon the demand of the market node compared to the potential of the country
     demand_factor = mean_demand[market_node] / sum([mean_demand[market_node_in_country] for market_node_in_country in utils.get_country_property(country_code, "market_nodes")])
     ires_potential_market_node = demand_factor * ires_potential_country
@@ -31,5 +27,7 @@ def get_potential_per_ires_node(market_node, ires_technology, *, mean_demand, co
     ires_data = utils.read_temporal_data(utils.path("input", "scenarios", config["scenario"], "ires", f"{market_node}.csv"))
     ires_nodes_in_market_node_count = len([column for column in ires_data.columns if column.startswith(f"{ires_technology}_")])
 
-    # Return the potential of the market node divided by the number of IRES nodes in the market node
-    return ires_potential_market_node / ires_nodes_in_market_node_count
+    # Return the potential of the market node divided by the number of IRES nodes in the market node (with a minimum of the currently installed capacity (only required for Italy))
+    current_capacity_ires_node = utils.get_current_capacity_per_ires_node(market_node, ires_technology, config=config)
+    potential_capacity_ires_node = ires_potential_market_node / ires_nodes_in_market_node_count
+    return max(current_capacity_ires_node, potential_capacity_ires_node)
