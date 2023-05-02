9ed0e4e2c2faa7e90e2d86c0d489b966646feb1f
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-05-02 16:41:54 +0200

    Update the width and height of the chart

--- chart/chart.py
+++ chart/chart.py
@@ -17,8 +17,8 @@ class Chart:
         self.ncols = ncols
 
         # Create the figure
-        width = (10 if wide else 6) * ncols ** (1 / 2)
-        height = 3.5 * nrows ** (1 / 2)
+        width = 6 * 522 / 252 if wide else 6 # 522/252 is the textwidth/columnwidth ratio in the Elsevier template
+        height = 3.5 * nrows ** (4 / 7)
         self.fig, self.axs = plt.subplots(nrows, ncols, figsize=(width, height), sharex=sharex, sharey=sharey)
 
         # Enable tight_layout
