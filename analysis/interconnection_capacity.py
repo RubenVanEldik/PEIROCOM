2b3e00561c2b5b2c7764411da51557d8c47daf65
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-05-09 13:13:32 +0200

    Show both the HVAC and HVDC connection expansions in the same graph

--- analysis/interconnection_capacity.py
+++ analysis/interconnection_capacity.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 
 import chart
@@ -20,35 +21,42 @@ def interconnection_capacity(output_directory):
         st.warning("This analysis is only available for runs where the interconnections are individually optimized")
         return
 
-    # Select the interconnection type
-    interconnection_type = st.sidebar.selectbox("Interconnection type", ["hvac", "hvdc"], format_func=utils.format_str)
+    # Check if the data should be aggregated per country
+    aggregrate_per_country = st.sidebar.checkbox("Aggregate per country")
 
     # Read the interconnection capacity
-    data = utils.read_csv(output_directory / "capacity" / "interconnections" / f"{interconnection_type}.csv")
+    all_data = pd.DataFrame()
+    for interconnection_type in ["hvac", "hvdc"]:
+        data = utils.read_csv(output_directory / "capacity" / "interconnections" / f"{interconnection_type}.csv")
 
-    number_of_shown_interconnections = st.sidebar.slider("Number of interconnections shown", value=10, min_value=3, max_value=len(data.index))
+        if aggregrate_per_country:
+            # Change the market nodes to countries
+            data["from"] = data["from"].apply(lambda market_node: utils.get_country_property(market_node[:2], "name"))
+            data["to"] = data["to"].apply(lambda market_node: utils.get_country_property(market_node[:2], "name"))
 
-    if st.sidebar.checkbox("Aggregate per country"):
-        # Change the market nodes to countries
-        data["from"] = data["from"].apply(lambda market_node: utils.get_country_property(market_node[:2], "name"))
-        data["to"] = data["to"].apply(lambda market_node: utils.get_country_property(market_node[:2], "name"))
+        # Group the data per connection
+        data["new_index"] = data["from"] + " to " + data["to"]
+        data = data.groupby(["new_index"]).sum(numeric_only=True)
+        data["interconnection_type"] = interconnection_type
 
-    # Group the data per connection
-    data["new_index"] = data["from"] + " to " + data["to"]
-    data = data.groupby(["new_index"]).sum(numeric_only=True)
+        all_data = pd.concat([all_data, data])
 
     # Get and sort all interconnections that got extra capacity
-    interconnections_with_extra_capacity = data.sort_values("extra").tail(number_of_shown_interconnections)
+    number_of_shown_interconnections = st.sidebar.slider("Number of interconnections shown", value=10, min_value=3, max_value=len(all_data.index))
+    interconnections_with_extra_capacity = all_data.sort_values("extra").tail(number_of_shown_interconnections)
 
     # Set the interconnection capacity to GW if the maximum extra capacity exceeds 1 GW
     unit = "MW"
     if interconnections_with_extra_capacity.extra.max() > 1000:
-        interconnections_with_extra_capacity /= 1000
+        interconnections_with_extra_capacity[["current", "extra"]] /= 1000
         unit = "GW"
 
     # Create the bar chart
     bar_chart = chart.Chart(xlabel=f"Extra capacity ({unit})", ylabel="")
-    bar_chart.axs.barh(interconnections_with_extra_capacity.index, interconnections_with_extra_capacity.extra, color=colors.primary())
+    bar_colors = [colors.primary() if interconnection_type == "hvdc" else colors.secondary() for interconnection_type in interconnections_with_extra_capacity["interconnection_type"]]
+    labels = [utils.format_str(interconnection_type) for interconnection_type in interconnections_with_extra_capacity["interconnection_type"]]
+    bar_chart.axs.barh(interconnections_with_extra_capacity.index, interconnections_with_extra_capacity.extra, color=bar_colors, label=labels)
+    bar_chart.add_legend()
 
     # Show the bar chart
     bar_chart.display()
