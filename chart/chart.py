31ac8eaec2703a7bb28d3ca92094a6a77d24e4a1
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-05-09 19:13:06 +0200

    Improve the logic of the chart width

--- chart/chart.py
+++ chart/chart.py
@@ -18,7 +18,7 @@ class Chart:
         self.wide = wide
 
         # Create the figure
-        width = 6 * 522 / 252 if wide else 6 # 522/252 is the textwidth/columnwidth ratio in the Elsevier template
+        width = 6 * ((522 / 252) if wide else 1) # 522/252 is the textwidth/columnwidth ratio in the Elsevier template
         height = 3.5 * nrows ** 0.5
         self.fig, self.axs = plt.subplots(nrows, ncols, figsize=(width, height), sharex=sharex, sharey=sharey)
 
