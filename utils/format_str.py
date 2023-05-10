31b4160a7e5fbd153414cf86b58e74bedfa2f125
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2023-05-10 12:34:13 +0200

    Re-add the underscores for the multi-word technologies

--- utils/format_str.py
+++ utils/format_str.py
@@ -14,6 +14,10 @@ def format_str(string):
     # Replace underscores with spaces
     string = string.replace("_", " ")
 
+    # Re-add the underscores for the multi-word technologies
+    string = string.replace("h2 ccgt", "h2_ccgt")
+    string = string.replace("h2 gas turbine", "h2_gas_turbine")
+
     # Format technology names and abbreviations properly, and capitalize the first word of the string
     word_list = []
     for index, word in enumerate(string.split(" ")):
